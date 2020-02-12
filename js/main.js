100e5d4bc4dd88f6ed937968ad9a9b984a4b10b6
Nyakinyua <wanyakinyua968@gmail.com>
2020-02-12 05:13:53 +0300

    Edit the services section to include description of services offered

--- js/main.js
+++ js/main.js
@@ -115,3 +115,9 @@
 	});
 
 })(jQuery);
+$(function() {
+	$('a[href*=#]').on('click', function(e) {
+	  e.preventDefault();
+	  $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+	});
+  });
